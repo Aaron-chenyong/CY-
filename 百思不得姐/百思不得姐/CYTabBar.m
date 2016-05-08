f500278e8ab2714bcd9500c4600cd757b5fea1ca
chenyong <chenyong@pro.local>
2016-05-08 17:42:17 +0800

    自定义tabbar

--- 百思不得姐/百思不得姐/CYTabBar.m
+++ 百思不得姐/百思不得姐/CYTabBar.m
@@ -31,4 +31,40 @@
 }
 
 
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    
+    //设置发布按钮的frame
+    self.publishButton.bounds = CGRectMake(0, 0, self.publishButton.currentBackgroundImage.size.width, self.publishButton.currentBackgroundImage.size.height);
+    self.publishButton.center = CGPointMake(self.frame.size.width * 0.5, self.frame.size.height * 0.5);
+    
+    //设置其他UITabBarButton的frame
+    CGFloat buttonY = 0;
+    CGFloat buttonW = self.frame.size.width / 5;
+    CGFloat buttonH = self.frame.size.height;
+    NSInteger index = 0;
+    for (UIView *button in self.subviews) {
+        if (![button isKindOfClass:[UIControl class]] || button == self.publishButton) {
+            continue;
+        }
+        //计算按钮的x值
+        CGFloat buttonX = buttonW * ((index > 1)?(index + 1):index);
+        button.frame = CGRectMake(buttonX, buttonY, buttonW, buttonH);
+        
+        //增加索引值
+        index ++;
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+
 @end
