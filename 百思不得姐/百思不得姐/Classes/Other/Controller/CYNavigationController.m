c29ebd2e0c4c948638cdde80a700e81a0f49c3b9
chenyong <chenyong@pro.local>
2016-05-10 21:56:52 +0800

    封装

--- 百思不得姐/百思不得姐/Classes/Other/Controller/CYNavigationController.m
+++ 百思不得姐/百思不得姐/Classes/Other/Controller/CYNavigationController.m
@@ -13,23 +13,57 @@
 @end
 
 @implementation CYNavigationController
+//当第一次使用这个类的时候会调用一次
++ (void)initialize
+{
+    //当导航栏用在XMGNavigationController中,appearance设置才会生效
+    //UINavigationBar *bar = [UINavigationBar appearanceWhenContainedIn:[self class], nil];
+    UINavigationBar *bar = [UINavigationBar appearance];
+    [bar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
+    
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-   
-    
-    // Do any additional setup after loading the view.
+    //[self.navigationBar setBackgroundImage:[UIImage imageNamed:@"navigationbarBackgroundWhite"] forBarMetrics:UIBarMetricsDefault];
 }
+
 /**
  *  可以在这个方法中拦截所有push进来的控制器
  */
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated
 {
-    [super pushViewController:viewController animated:animated];
+    if (self.childViewControllers.count > 0) {
+        UIButton * button = [UIButton buttonWithType:UIButtonTypeCustom];
+        [button setTitle:@"返回" forState:UIControlStateNormal];
+        [button setImage:[UIImage imageNamed:@"navigationButtonReturn"]  forState:UIControlStateNormal];
+        [button setImage:[UIImage imageNamed:@"navigationButtonReturnClick"] forState:UIControlStateHighlighted];
+        button.size = CGSizeMake(70, 30);
+        //让按钮内部的所有内存左对齐
+        button.contentHorizontalAlignment = UIControlContentHorizontalAlignmentLeft;
+        //[button sizeToFit];
+        button.contentEdgeInsets = UIEdgeInsetsMake(0, -10, 0, 0);
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
+        [button addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+        viewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
+        //隐藏tabbar
+        viewController.hidesBottomBarWhenPushed = YES;
+        
+    }
     
-    viewController.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"返回" style:UIBarButtonItemStyleDone target:nil action:nil];
+    //这句super的push要放在后面,让viewController可以覆盖上面设置的leftBarButtonItem
+    [super pushViewController:viewController animated:animated];
 
 }
+
+- (void)back
+{
+    [self popViewControllerAnimated:YES];
+
+}
+
 
 @end
